df441ef65bbd33ac7310aa231abf0deb9024a1f4
changhyeonO <ckdgus5189@gmail.com>
2023-04-09 23:47:59 +0900

    nodemon으로 자동 서버 재시동하기

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.listen(port, function () {
 
 app.get('/pet', function (req, res) {
     res.send("펫 용품 쇼핑할 수 있는 페이지입니다.")
-})
+});
 
 app.get('/beauty', function (req, res) {
     res.send("뷰티 용품 쇼핑할 수 있는 페이지입니다.")
-})
+});
