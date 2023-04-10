bbc08831ad6366ed221d604508a8fc2fc72dc6cf
changhyeonO <ckdgus5189@gmail.com>
2023-04-11 00:47:37 +0900

    mongoDB를 활용해 form에서 전송된 데이터 저장

--- server.js
+++ server.js
@@ -3,11 +3,37 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const MongoClient = require('mongodb').MongoClient;
 const port = 8080;
 
-app.listen(port, function () {
-    console.log(`listening on ${port}`)
-});
+let db;
+MongoClient.connect('mongodb+srv://colajelly:Jimin2448@cluster0.2knbyz0.mongodb.net/?retryWrites=true&w=majority', function (err, client) {
+    //연결되면 할일
+    if (err) {
+        return console.log(err)
+    };
+
+    db = client.db('todoapp');
+
+    db.collection('post').insertOne({ name: "changhyeon", age: 25 }, function (err, result) {
+        console.log('저장완료');
+    });
+
+    app.listen(port, function () {
+        console.log(`listening on ${port}`)
+    });
+
+
+    app.post('/add', function (req, res) {
+        res.send('전송완료!')
+        db.collection('post').insertOne({ name: req.body.title, date: req.body.date }, function (err, result) {
+            console.log('저장완료!')
+        })
+    });
+
+
+})
+
 
 app.get('/pet', function (req, res) {
     res.send("펫 용품 쇼핑할 수 있는 페이지입니다.")
@@ -25,8 +51,3 @@ app.get('/write', function (req, res) {
     res.sendFile(__dirname + "/write.html")
 });
 
-app.post('/add', function (req, res) {
-    res.send('전송완료!')
-    console.log(req.body.title)
-    console.log(req.body.date)
-})
